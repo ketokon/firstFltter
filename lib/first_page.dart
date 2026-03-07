f21157a181ac41d1169c1fcbdffe6b78e1f2bbec
Kengo Nemoto <ketokon@owlan031075232.sras.sic.shibaura-it.ac.jp>
2026-03-07 11:30:09 +0900

    YouTubeの画面模倣

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -3,29 +3,82 @@ import 'package:first_program/second_page.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
-  final List<int> entries = [0,1,2,3];
+  final List<String> entries = [
+    'NIKKEモーション紹介-E.H.｜勝利の女神：NIKKE',
+    '『ニケラジ』Vol.9-ゲスト：久野美咲(ココア役)｜勝利の女神：NIKKE',
+    '公式Xアカウント100万フォロワー突破記念 お祝い二次創作｜勝利の女神：NIKKE',
+    'マストの特別なおもてなし｜勝利の女神：NIKKE',
+    'キャラモーション紹介 - クルミ(Kurumi) │『勝利の女神：NIKKE』×『リコリス・リコイル』',
+    'キャラモーション紹介 - 井ノ上たきな(Takina Inoue) │『勝利の女神：NIKKE』×『リコリス・リコイル』',
+    'キャラモーション紹介 - 錦木千束(Chisato Nishikigi) │『勝利の女神：NIKKE』×『リコリス・リコイル』'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent,
-          title: Text('滑らかラテSmart')
+        title: Text(
+          '勝利の女神：NIKKE',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white60,
+        actions: [
+          Icon(Icons.ondemand_video_outlined),
+          SizedBox(width: 24),
+          Icon(Icons.search),
+          SizedBox(width: 24),
+          Icon(Icons.menu),
+          SizedBox(width: 24),
+        ],
       ),
-      body: ListView.separated(
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(
-                child: Container(
-                  color: Colors.yellow,
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'No ${entries[index]}',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ));
-          }, separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.black,),
-    )
+      body: Container(
+        color: Colors.white,
+        child: ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index){
+              return Container(
+                height: 100,
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Image.network(
+                        'https://i.ytimg.com/vi/CAUh7EREOjE/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLApQKuENOM0BiIzWjqtaAEQUvbCfw',
+                      height: 100,
+                    ),
+                    SizedBox(width: 8,),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            entries[index],
+                            style: TextStyle(
+                              color: Colors.black,
+                              height: 1.3,
+                              fontSize: 15,
+                            ),
+                            maxLines: 3,
+                          ),
+                          Text(
+                            '3.5万回視聴:2日前',
+                            style: TextStyle(
+                              color: Colors.grey[800],
+                              fontSize: 12,
+                            ),
+
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+      ),
+      ),
     );
   }
 }
